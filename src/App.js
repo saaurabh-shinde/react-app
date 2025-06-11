5ccef2992c40797f2c276baefa9748ae05b4b830
Jagadish-shinde <jagdish.shinde@polariscampus.com>
2025-06-11 18:38:50 +0530

    add tutor

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With ME !!!
+          Learn React With ME (Akash)!
         </a>
       </header>
     </div>
